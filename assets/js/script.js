dafdcdd72fd493f9f01393ae3102c549b04284a7
IbrahimDev04 <tu7nluy9f@code.edu.az>
2024-04-28 16:58:46 +0400

    final form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,129 @@
+
 fetch('https://api.tvmaze.com/shows')
     .then( res => res.json())
     .then( a => {
         getMovie(a);
     })
 
+function SortingElement(data){
+
+    let sortOrder = document.getElementById("sort-choise").value;
+    console.log(sortOrder)
+    fetch('https://api.tvmaze.com/shows')
+    .then( res => {return res.json()})
+    .then( data => {
+        if (sortOrder === "to-low"){
+            data.sort((a,b) => b.rating.average - a.rating.average)
+            getMovie(data)
+        }
+        else if(sortOrder === "to-high"){
+            data.sort((a,b) => a.rating.average - b.rating.average)
+            getMovie(data)
+        }
+        else if(sortOrder === "rating"){
+            getMovie()
+        }
+    })
+
+
+
+}
+
+window.onkeyup = keyup;
+var inputTextValue;
+var queryUrl
+function keyup(e) {
+    inputTextValue = e.target.value;
+  
+    if (e.keyCode == 13) {
+        queryUrl = `https://api.tvmaze.com/search/shows?q=` + inputTextValue;
+        fetch(queryUrl)
+            .then(res => res.json())
+            .then(data => getMovieSearching(data))
+
+            var area = document.getElementById("movie-area")
+
+            function getMovieSearching(movies){
+                area.innerHTML = ""
+                movies.forEach(moviess => {
+
+                    
+                    if(moviess.show.image == null){
+                        return;
+                    }
+
+                    if(moviess.show.rating.average == null){
+                        return;
+                   }
+                    
+                    area.innerHTML += 
+                    `
+                             <div class="col-md-3 mg-bottom">
+                                <div class="card position-relative">
+                                <span style="z-index:4; left: 30px; top: 30px; border: none !important; border-radius: 10px; color: white;" class="position-absolute translate-middle p-2 bg-primary border ">
+                                <span>${moviess.show.rating.average}</span>
+                            </span>
+                                    <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
+                                    <img src="${moviess.show.image.medium}" class="img-fluid img-size width: 320px; height: 450px;"/>
+                                    <a href="detail.html?${moviess.show.id}">
+                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                    </a>
+                                    </div>
+                                    <div class="card-body max-lenght width" style="background-color: rgb(207, 121, 29);">
+                                    <h5 class="card-title"><b>${moviess.show.name}</b></h5>
+                                    <p class="card-text max-lenght">${moviess.show.summary}</p>
+                                    <a href="detail.html?${moviess.show.id}" class="btn btn-primary" data-mdb-ripple-init>Button</a>
+                                    </div>
+                                </div>
+                            </div>
+                    `
+                });
+            }
+        
+    }
+  }
+    
+
 
 var area = document.getElementById("movie-area")
 
 
-var pagerSize = 2;
-var pageLast = 0;       
-
-load(emp.employees, pageLast, pagerSize);
-
-$('#loader').click(function(){
-   load(emp.employees, pageLast, pagerSize);
-});
-
-function load(data,start,limit){  
-  var employees = []; 
-  for(var i = start; i < start+limit; i++) {
-      var object = data[i];
-      var newElement = $('<p/>').attr("id", "emp"+i).text(object.firstName+' '+object.lastName)
-      employees.push(newElement);
-  }
-  $('.js-load').append(employees);
-  $('.js-load').append("<hr>");
-  pageLast+=limit;
-}
 
 
-function getMovie(movies,){
 
+function getMovie(movies){
+    area.innerHTML = ""
     movies.forEach(moviess => {
+
+       if(moviess.rating.average == null){
+            return;
+       }
         
         area.innerHTML += 
         `
                  <div class="col-md-3 mg-bottom">
-                    <div class="card">
+                    <div class="card position-relative">
+                    <span style="z-index:4; left: 30px; top: 30px; border: none !important; border-radius: 10px; color: white;" class="position-absolute translate-middle p-2 bg-primary border ">
+                        <span>${moviess.rating.average}</span>
+                    </span>
                         <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
                         <img src="${moviess.image.medium}" class="img-fluid img-size width: 320px; height: 450px;"/>
-                        <a href="${moviess.url}">
+                        <a href="detail.html?${moviess.id}">
                             <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
                         </a>
                         </div>
                         <div class="card-body max-lenght width" style="background-color: rgb(207, 121, 29);">
                         <h5 class="card-title"><b>${moviess.name}</b></h5>
-                        <p class="card-text max-lenght">${moviess.summary} IMDB : ${moviess.externals.imdb}</p>
-                        <a href="${moviess.url}" class="btn btn-primary" data-mdb-ripple-init>About</a>
+                        <p class="card-text max-lenght">${moviess.rating.average}</p>
+                        <a href="detail.html?${moviess.id}" class="btn btn-primary" data-mdb-ripple-init>Button</a>
                         </div>
                     </div>
                 </div>
         `
     });
 }
+
+
 
 
 
@@ -65,7 +135,60 @@ function getMovie(movies,){
 //     })
 
 
+// window.onkeyup = keyup;
+// var inputTextValue;
+// var queryUrl
+// function keyup(e) {
+//     inputTextValue = e.target.value;
+  
+//     if (e.keyCode == 13) {
+//         queryUrl = `https://api.tvmaze.com/search/shows?q=` + inputTextValue;
+//         fetch(queryUrl)
+//             .then(res => res.json())
+//             .then(data => getMovieSearching(data))
+
+//             var area = document.getElementById("movie-area")
+
+//             function getMovieSearching(movies){
+//                 area.innerHTML = ""
+//                 movies.forEach(moviess => {
+
+                    
+//                     if(moviess.show.image == null){
+//                         return;
+//                     }
+                    
+//                     area.innerHTML += 
+//                     `
+//                              <div class="col-md-3 mg-bottom">
+//                                 <div class="card">
+//                                     <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
+//                                     <img src="${moviess.show.image.medium}" class="img-fluid img-size width: 320px; height: 450px;"/>
+//                                     <a href="">
+//                                         <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+//                                     </a>
+//                                     </div>
+//                                     <div class="card-body max-lenght width" style="background-color: rgb(207, 121, 29);">
+//                                     <h5 class="card-title"><b>${moviess.show.name}</b></h5>
+//                                     <p class="card-text max-lenght">${moviess.show.summary}</p>
+//                                     <a href="#!" class="btn btn-primary" data-mdb-ripple-init>Button</a>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                     `
+//                 });
+//             }
+        
+//     }
+//   }
+    
+
+
 // var area = document.getElementById("movie-area")
+
+
+
+
 
 // function getMovie(movies){
 
@@ -91,3 +214,6 @@ function getMovie(movies,){
 //         `
 //     });
 // }
+
+
+
